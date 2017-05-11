0ea138d25e90205bc212467251291963e1202f95
Marcin Nowicki <pr0d1r2@gmail.com>
2017-05-11 09:32:36 +0200

    Run something once a day

---
+++ run_once_a_day.sh
@@ -0,0 +1,12 @@
+function run_once_a_day() {
+  local run_once_a_day_MARKER=`run_once_a_day_marker $@`
+  if [ -e $run_once_a_day_MARKER ]; then
+    case `dateh` in
+      `cat $run_once_a_day_MARKER`)
+        return 0
+        ;;
+    esac
+  fi
+  $@ || return $?
+  dateh > $run_once_a_day_MARKER || return $?
+}
---
+++ run_once_a_day_marker.sh
@@ -0,0 +1,4 @@
+function run_once_a_day_marker() {
+  run_once_a_day_marker_ID=`echo $@ | md5sum | cut -f 1 -d ' '`
+  echo /tmp/.run_once_a_day_marker-$run_once_a_day_marker_ID || return $?
+}
