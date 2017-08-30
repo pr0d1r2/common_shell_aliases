34e2631d6a1f27102400583f03217e932b6ae69a
Marcin Nowicki <pr0d1r2@gmail.com>
2017-08-30 05:44:14 +0200

    Use GNU parallel for parallel operations

--- kill_forcefully.sh
+++ kill_forcefully.sh
@@ -1,14 +1,5 @@
 function kill_forcefully() {
-  local kill_forcefully_PID
-  for kill_forcefully_PID in $@
-  do
-    echorun kill $kill_forcefully_PID &
-  done
-  sleep 1
-  wait
-  for kill_forcefully_PID in $@
-  do
-    echorun kill -9 $kill_forcefully_PID &
-  done
-  wait
+  echorun parallel "kill {}" ::: "$@" 2>/dev/null
+  echorun sleep 1
+  echorun parallel "kill -9 {}" ::: "$@" 2>/dev/null
 }
