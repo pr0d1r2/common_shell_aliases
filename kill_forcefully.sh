056c98b1fa10b3dc042c6b6e02e1b124ca732341
Marcin Nowicki <pr0d1r2@gmail.com>
2017-09-03 08:32:30 +0200

    Skip verbosity. Do not run when no pids

--- kill_forcefully.sh
+++ kill_forcefully.sh
@@ -1,5 +1,6 @@
 function kill_forcefully() {
-  echorun parallel "kill {}" ::: "$@" 2>/dev/null
-  echorun sleep 1
-  echorun parallel "kill -9 {}" ::: "$@" 2>/dev/null
+  test -n "$1" || return 0
+  parallel "kill {}" ::: "$@" 2>/dev/null
+  sleep 1
+  parallel "kill -9 {}" ::: "$@" 2>/dev/null
 }
