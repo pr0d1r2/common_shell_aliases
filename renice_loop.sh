c45a616b2d5f006577fcc63381f3638b64b08309
Marcin Nowicki <pr0d1r2@gmail.com>
2017-09-04 04:55:06 +0200

    Use GNU parallel for renicing in loop

--- renice_loop.sh
+++ renice_loop.sh
@@ -1,7 +1,6 @@
 function renice_loop() {
   local renice_loop_LEVEL="-18"
-  local renice_loop_INTERVAL=5
-  local renice_loop_PROCESS_NAME
+  local renice_loop_INTERVAL="5"
   case $NICE in
     -[1-9] | 1[0-8] | [0-9] | 1[0-9] | 20)
       renice_loop_LEVEL=$NICE
@@ -13,11 +12,12 @@ function renice_loop() {
       ;;
   esac
   while true; do
-    for renice_loop_PROCESS_NAME in $@
-    do
-      NICE="$renice_loop_LEVEL" renice_named $renice_loop_PROCESS_NAME
-    done
-    sleep $renice_loop_INTERVAL
+    sudo echo -n
+    parallel \
+      "ps -ax | pgrep {} | parallel -I '<>' 'sudo renice $renice_loop_LEVEL <>'" \
+      ::: \
+      "$@" 2>/dev/null
+    sleep "$renice_loop_INTERVAL"
     echo
   done
 }
