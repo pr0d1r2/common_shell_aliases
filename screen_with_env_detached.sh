ad2b87f785b798853cf2375776f1d5ffdf9407ac
Marcin Nowicki <pr0d1r2@gmail.com>
2017-11-11 03:23:18 +0100

    Improve screen commands

--- screen_with_env_detached.sh
+++ screen_with_env_detached.sh
@@ -1,9 +1,22 @@
 function screen_with_env_detached() {
-  echo "#!/bin/bash
-    source $HOME/.bash_profile
+  local screen_with_env_detached_PROFILE
+  case $(basename "$SHELL") in
+    zsh)
+      screen_with_env_detached_PROFILE="$HOME/.zshrc"
+      ;;
+    bash)
+      screen_with_env_detached_PROFILE="$HOME/.bash_profile"
+      ;;
+  esac
+  if [ ! -e "$screen_with_env_detached_PROFILE" ]; then
+    echo "No $screen_with_env_detached_PROFILE file !!!"
+    return 1
+  fi
+  echo "#!$SHELL
+    source $screen_with_env_detached_PROFILE
     $@
     echo
-    echo "Press ENTER to leave from shell."
+    echo 'Press ENTER to leave from shell.'
     read X
   " > /tmp/.screen_with_env_detached-$$.sh
   chmod 700 /tmp/.screen_with_env_detached-$$.sh
--- screen_with_env_detached_named.sh
+++ screen_with_env_detached_named.sh
@@ -1,9 +1,22 @@
 function screen_with_env_detached_named() {
-  echo "#!/bin/bash
-    source $HOME/.bash_profile
+  local screen_with_env_detached_named_PROFILE
+  case $(basename "$SHELL") in
+    zsh)
+      screen_with_env_detached_named_PROFILE="$HOME/.zshrc"
+      ;;
+    bash)
+      screen_with_env_detached_named_PROFILE="$HOME/.bash_profile"
+      ;;
+  esac
+  if [ ! -e "$screen_with_env_detached_named_PROFILE" ]; then
+    echo "No $screen_with_env_detached_named_PROFILE file !!!"
+    return 1
+  fi
+  echo "#!$SHELL
+    source $screen_with_env_detached_named_PROFILE
     ${@:2}
     echo
-    echo "Press ENTER to leave from shell."
+    echo 'Press ENTER to leave from shell.'
     read X
   " > /tmp/.screen_with_env_detached_named-$$.sh
   chmod 700 /tmp/.screen_with_env_detached_named-$$.sh
