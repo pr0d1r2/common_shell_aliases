317c8f8eac62b0fce6bc134c85727098aee480a9
Marcin Nowicki <pr0d1r2@gmail.com>
2017-12-05 13:13:27 +0100

    Simplify implementation

--- cpu_list.sh
+++ cpu_list.sh
@@ -3,10 +3,6 @@
 # For example, clonning parallel test databases fast:
 # cpu_list | parallel "dropdb app_test{}; createdb app_test{} -T app_test -O app"
 function cpu_list() {
-  local cpu_list_NUM=1
-  local cpu_list_NPROC=`nproc`
-  while [ $cpu_list_NUM -le $cpu_list_NPROC ]; do
-    echo $cpu_list_NUM
-    cpu_list_NUM=`expr $cpu_list_NUM + 1`
-  done
+  seq "$(nproc)"
+  return $?
 }
