493c5fa0f217fe449a3cad4eabe4e9b96371a886
Marcin Nowicki <pr0d1r2@gmail.com>
2018-01-04 08:03:23 +0100

    Use better parallelism and syntax

--- gz_to_xz_in_directory.sh
+++ gz_to_xz_in_directory.sh
@@ -1,10 +1,5 @@
 function gz_to_xz_in_directory() {
-  local gz_to_xz_in_directory_DIR
-  for gz_to_xz_in_directory_DIR in $@
-  do
-    if [ -d $gz_to_xz_in_directory_DIR ]; then
-      find $gz_to_xz_in_directory_DIR -type f -iname "*.gz" | \
-        parallel 'echo "{}" "=>" "{= s/.gz$/.xz/ =}" ; gzip -cd "{}" | xz -c9 > "{= s/.gz$/.xz/ =}" && rm -f "{}"'
-   fi
-  done
+  parallel \
+    'test -d {} && find {} -type f -iname "*.gz"' \
+      parallel 'echo "{}" "=>" "{= s/.gz$/.xz/ =}" ; gzip -cd "{}" | xz -c9 > "{= s/.gz$/.xz/ =}" && rm -f "{}"'
 }
