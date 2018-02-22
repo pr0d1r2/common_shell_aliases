64a12be9851a61c745a50eeefd4ce1c58effe9f2
Marcin Nowicki <pr0d1r2@gmail.com>
2018-02-22 06:54:04 +0100

    Conform to ShellCheck

--- benchmark.sh
+++ benchmark.sh
@@ -1,10 +1,12 @@
 benchmark() {
-  local STARTTIME=`gdate +%s%N` ##Darwin
-  local STARTTIME=`date +%s%N` ##Linux
-  $@
-  local FINISHTTIME=`gdate +%s%N` ##Darwin
-  local STARTTIME=`date +%s%N` ##Linux
+  local benchmark_STARTTIME
+  local benchmark_FINISHTTIME
+  benchmark_STARTTIME=$(gdate +%s%N) ##Darwin
+  benchmark_STARTTIME=$(date +%s%N) ##Linux
+  "$@"
+  benchmark_FINISHTTIME=$(gdate +%s%N) ##Darwin
+  benchmark_FINISHTTIME=$(date +%s%N) ##Linux
   echo '---'
   echo 'Running time (seconds):'
-  echo "scale=5; ($FINISHTTIME - $STARTTIME) / 1000000000.0" | bc
+  echo "scale=5; ($benchmark_FINISHTTIME - $benchmark_STARTTIME) / 1000000000.0" | bc
 }
