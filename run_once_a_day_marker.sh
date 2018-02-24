9d3e8fba6270ed36ae3ef874468d279bc4eb0659
Marcin Nowicki <pr0d1r2@gmail.com>
2018-02-24 06:36:20 +0100

    Conform to ShellCheck

--- run_once_a_day_marker.sh
+++ run_once_a_day_marker.sh
@@ -1,4 +1,4 @@
 function run_once_a_day_marker() {
-  run_once_a_day_marker_ID=`echo $@ | md5sum | cut -f 1 -d ' '`
-  echo /tmp/.run_once_a_day_marker-$run_once_a_day_marker_ID || return $?
+  run_once_a_day_marker_ID=$(echo "$@" | md5sum | cut -f 1 -d ' ')
+  echo "/tmp/.run_once_a_day_marker-$run_once_a_day_marker_ID" || return $?
 }
