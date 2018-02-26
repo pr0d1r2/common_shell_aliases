fe235dc2c13492a30b9054b9672ef5a3a73098db
Marcin Nowicki <pr0d1r2@gmail.com>
2018-02-26 06:32:30 +0100

    Conform to ShellCheck

--- run_once_a_day.sh
+++ run_once_a_day.sh
@@ -1,12 +1,14 @@
 function run_once_a_day() {
-  local run_once_a_day_MARKER=`run_once_a_day_marker $@`
-  if [ -e $run_once_a_day_MARKER ]; then
-    case `dateh` in
-      `cat $run_once_a_day_MARKER`)
+  local run_once_a_day_MARKER
+  run_once_a_day_MARKER=$(run_once_a_day_marker "$@")
+  if [ -e "$run_once_a_day_MARKER" ]; then
+    case $(dateh) in
+      $(cat "$run_once_a_day_MARKER"))
         return 0
         ;;
     esac
   fi
+  # shellcheck disable=SC2068
   $@ || return $?
-  dateh > $run_once_a_day_MARKER || return $?
+  dateh > "$run_once_a_day_MARKER" || return $?
 }
