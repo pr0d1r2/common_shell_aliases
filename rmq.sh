6cb934ed732209ce90675ac688a79948e74b6af1
Marcin Nowicki <pr0d1r2@gmail.com>
2020-01-27 17:45:16 +0200

    Allow quickly remove a big directory

---
+++ rmq.sh
@@ -0,0 +1,14 @@
+# ReMove Quickly
+#
+# Renames a directory and start removing it in background.
+# Useful for huge directories.
+#
+# Example usage:
+#   rmq node_modules && yarn
+function rmq() {
+  parallel \
+    "mv {} {}-deleting-$$ && screen -d -m -S {}-deleting-$$ rm -rf {}-deleting-$$" \
+    :::
+    "$@"
+  return $?
+}
